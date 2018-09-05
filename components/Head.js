688c8a7e8adc663494b949dd60a7d11daf5d0279
Matthew Elphick <matthew.elphick@threadsstyling.com>
2018-09-05 14:26:04 +0100

    :eyes:

--- components/Head.js
+++ components/Head.js
@@ -4,6 +4,9 @@ export default () => (
   <Head>
     <title>FFXIV Pocketcraft</title>
     <meta name='viewport' content='initial-scale=1.0, width=device-width' key='viewport' />
+    <meta name='keywords' content='Final Fantasy, crafting, XIV, gaming, tool' />
+    <meta name='description' content='A tool for Final Fantasy XIV to help craft using your avaliable items' />
+    <meta name='robots' content='index' />
     <link rel='stylesheet' type='text/css' href='/public/css/bulma.css' />
     <link rel='stylesheet' type='text/css' href='/public/styles.css' />
     <script src='https://secure.xivdb.com/tooltips.js' />
